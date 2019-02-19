8edab5702d11961e33be6e333f8bfa63c8189c12
lily <lilinyu861@outlook.com>
2019-02-19 19:23:12 +0800

    添加发送邮箱

---
+++ common/sendEmail.py
@@ -0,0 +1,54 @@
+# email：3381350680@qq.com password：abc123456
+
+
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import time
+
+
+# 自动发送邮件
+class SendEmail():
+    def send_email(self, new_report):
+        # 读取测试报告中的内容作为邮件的内容
+        with open(new_report, 'r', encoding='utf8') as f:
+            mail_body = f.read()
+        # 发件人地址
+        send_addr = '3381350680@qq.com'
+        # 收件人地址
+        reciver_addr = '3381350680@qq.com'
+        # 发送邮箱的服务器地址 qq邮箱是'smtp.qq.com', 136邮箱是'smtp.136.com'
+        mail_server = 'smtp.qq.com'
+        now = time.strftime("%Y-%m-%d %H_%M_%S")
+        # 邮件标题
+        subject = 'web自动化测试报告测试报告' + now
+        # 发件人的邮箱及邮箱授权码
+        username = '3381350680@qq.com'
+        password = '**'  # 注意这里是邮箱的授权码而不是邮箱密码
+        # 邮箱的内容和标题
+        message = MIMEText(mail_body, 'html', 'utf8')
+        message['Subject'] = Header(subject, charset='utf8')
+        # 发送邮件
+        smtp = smtplib.SMTP()
+        smtp.connect(mail_server)
+        smtp.login(username, password)
+        smtp.sendmail(send_addr, reciver_addr.split(','), message.as_string())
+        smtp.quit()
+
+    # 获取最新报告的地址
+    def acquire_report_address(self, reports_address):
+        # 测试报告文件夹中的所有文件加入到列表
+        test_reports_list = os.listdir(reports_address)
+        # 按照升序排序生成新的列表
+        new_test_reports_list = sorted(test_reports_list)
+        # 获取最新的测试报告
+        the_last_report = new_test_reports_list[-1]
+        # 最新的测试报告地址
+        the_last_report_address = os.path.join(reports_address, the_last_report)
+        return the_last_report_address
+
+
+
+
+
--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import unittest
 import HTMLTestRunner
 import time
+from common.sendEmail import SendEmail
 
 
 def get_test_cases(dirpath):
+    # dirpath 是存放测试用例的文件路径
     test_cases = unittest.TestSuite()
+    # 测试用例均使用"test_"开头命名
     suites = unittest.defaultTestLoader.discover(dirpath, 'test_*.py', top_level_dir=dirpath)
     for suite in suites:
         test_cases.addTests(suite)
@@ -14,7 +17,14 @@ def get_test_cases(dirpath):
 if __name__ == '__main__':
     cases = get_test_cases('../testcase')
     now = time.strftime("%Y-%m-%d %H_%M_%S")
+    test_reports_address = '../report'
     filename = '../report/' + now + 'report.html'  # 设置报告文件名
     f = open(filename, 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream=f, title=u'TEST', description=u'详细测试结果如下:')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=f, title=u'Web自动化测试', description=u'详细测试结果如下:')
     runner.run(cases)
+    f.close()
+    time.sleep(6)
+    # 查找最新生成的测试报告地址
+    new_report_addr = SendEmail().acquire_report_address(test_reports_address)
+    # 自动发送邮件
+    SendEmail().send_email(new_report_addr)
